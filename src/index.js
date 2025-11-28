b2e97aad30f629e8318c08b65a0721538d3a8e26
Ayubiy1 <ayubiyurinboyev@gmail.com>
2025-11-28 16:02:52 +0500

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -27,11 +27,6 @@ app.use(
   })
 );
 
-// OPTIONS preflight fix
-app.options("*", (req, res) => {
-  res.sendStatus(200);
-});
-
 // Routes
 app.use("/api/cart", korzinkaRouter);
 app.use("/api/auth", authRoutes);
@@ -50,54 +45,3 @@ mongoose
     );
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
-
-{
-  // const express = require("express");
-  // const mongoose = require("mongoose");
-  // const cors = require("cors");
-  // const cookieParser = require("cookie-parser");
-  // require("dotenv").config();
-  // const authRoutes = require("./routes/auth"); // auth.js router
-  // const userRoutes = require("./routes/userRoutes"); // agar users.js bo‘lsa shu yerga
-  // const categoryRoutes = require("./routes/categoryRoutes");
-  // const productRoutes = require("./routes/productRoutes");
-  // const coruselRoutes = require("./routes/corusel");
-  // const korzinkaRouter = require("./routes/korzinkaRouter");
-  // const app = express();
-  // // Middlewares
-  // app.use(express.json());
-  // app.use(cookieParser());
-  // // CORS setup
-  // app.use(
-  //   cors({
-  //     origin: "https://shopify-steel-two.vercel.app",
-  //     credentials: true,
-  //     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  //     allowedHeaders: ["Content-Type", "Authorization"],
-  //   })
-  // );
-  // app.options("*", cors());
-  // // app.use(
-  // //   cors({
-  // //     origin: ["https://shopify-steel-two.vercel.app"],
-  // //     credentials: true,
-  // //   })
-  // // );
-  // // Routes
-  // app.use("/api/cart", korzinkaRouter);
-  // app.use("/api/auth", authRoutes);
-  // app.use("/api/users", userRoutes);
-  // app.use("/api/categories", categoryRoutes);
-  // app.use("/api/products", productRoutes);
-  // app.use("/api/corusel", coruselRoutes);
-  // // MongoDB connect
-  // mongoose
-  //   .connect(process.env.MONGO_URI)
-  //   .then(() => {
-  //     console.log("✅ MongoDB connected");
-  //     app.listen(process.env.PORT || 3000, () =>
-  //       console.log(`🚀 Server running on port ${process.env.PORT || 3000}`)
-  //     );
-  //   })
-  //   .catch((err) => console.error("❌ MongoDB connection error:", err));
-}
