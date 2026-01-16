f2a831b339cf0bd27f2c28c7cd87038d7923f4de
Ayubiy1 <ayubiyurinboyev@email.com>
2026-01-16 16:08:54 +0500

    fixed: 16

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // CORS
 app.use(
   cors({
-    origin: ["https://shopify-steel-two.vercel.app", "http://localhost:5173"],
+    origin: ["https://shopify-steel-two.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
